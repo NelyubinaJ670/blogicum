468490e3f734f9c1dcb63584290b4dd4232960c6
Юлия Нелюбина <loginovskaya.yuliya.677@mail.ru>
2023-06-03 17:40:16 +0900

    установила Django Debug Toolbar

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
 
 ROOT_URLCONF = 'blogicum.urls'
 
